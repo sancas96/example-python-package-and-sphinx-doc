56dcd470552d41c8c0a8057543d1ae3310e13ea7
Erick Palacios Moreno <palmoreck@gmail.com>
2021-03-16 17:29:46 -0600

    fixing docstring newlines for funcs in module compute integral

--- src/opt2/integration/compute_integral.py
+++ src/opt2/integration/compute_integral.py
@@ -9,13 +9,19 @@ def Rcf(f,a,b,n):
     i=0,1,...,n-1 and h_hat=(b-a)/n
 
     Args:
-        f (function): function expression of integrand
-        a (float): left point of interval
-        b (float): right point of interval
-        n (float): number of subintervals
+    
+        f (function): function expression of integrand.
+        
+        a (float): left point of interval.
+        
+        b (float): right point of interval.
+        
+        n (float): number of subintervals.
+        
     Returns:
+    
         sum_res (float): numerical approximation to integral
-        of f in the interval a,b
+            of f in the interval a,b
     """
     h_hat=(b-a)/n
     nodes=[a+(i+1/2)*h_hat for i in range(0,n)]
@@ -28,17 +34,26 @@ def Rcf2(f,a,b,n):
     Similar function to Rcf just to show how two different docstrings
     in Python can be used with Sphinx (see source to compare).
     
-    :param f: function expression of integrand
-    :param a: left point of interval
-    :param b: right point of interval
-    :param n: float
-    :type f: function
-    :type a: float
-    :type b: float
-    :type n: float
+    :param f: function expression of integrand.
+    
+    :param a: left point of interval.
+    
+    :param b: right point of interval.
+    
+    :param n: float.
+    
+    :type f: function.
+    
+    :type a: float.
+    
+    :type b: float.
+    
+    :type n: float.
+    
     :returns: numerical approximation to integral of f in
-              the interval a,b
-    :rtype: float
+        the interval a,b.
+              
+    :rtype: float.
     """
     h_hat=(b-a)/n
     nodes=[a+(i+1/2)*h_hat for i in range(0,n)]
